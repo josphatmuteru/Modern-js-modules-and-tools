b339cd89a0e2489edadf8c65a2fb1583e3f9a50a
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-10-08 16:24:57 +0300

    learnt how to use the commandline, npm and installed cloneDeep

--- script.js
+++ script.js
@@ -13,8 +13,52 @@ console.log('Importing module');
 // console.log(shoppingCart.totalPrice);
 
 import add, { cart } from './shoppingCart.js';
-add('pizza', 2);
-add('bread', 5);
-add('melons', 6);
+// add('pizza', 2);
+// add('bread', 5);
+// add('melons', 6);
 
-console.log(cart);
+// console.log(cart);
+
+const shoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${product} ${quantity} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${product} ${quantity} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+shoppingCart2.addToCart('apple', 2);
+shoppingCart2.addToCart('pizza', 2);
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+
+console.log(stateClone);
+console.log(stateDeepClone);
