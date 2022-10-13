37a29405e79b9ae1ef097a49ca2c3df4cde36f01
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-10-13 11:36:00 +0300

    fixed more bad code

--- clean.js
+++ clean.js
@@ -1,4 +1,5 @@
-const budget = [
+'strict mode';
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,42 +8,82 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendinglimits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
-};
-const getLimit = user => spendinglimits?.[user] ?? 0;
-const addExpense = function (value, description, user = 'josphat') {
-  if (!user) user = 'jonas';
-  user = user.toLowerCase();
+});
+const getLimit = (limits, user) => limits?.[user] ?? 0;
 
-  if (value <= getLimit(user)) {
-    budget.push({ value: -value, description, user });
-  }
+//pure function
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'josphat'
+) {
+  const cleanUser = user.toLowerCase();
+
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : state;
+  // budget.push({ value: -value, description, cleanUser });
 };
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+
+const newBudget2 = addExpense(
+  budget,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+const newBudget3 = addExpense(newBudget1, spendingLimits, 200, 'Stuff', 'Jay');
+
+console.log(newBudget1);
+console.log(newBudget2);
 console.log(budget);
 
-const checkExpenses = function () {
-  for (const entry of budget)
-    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
-};
-checkExpenses();
+// const checkExpenses1 = function (state, limits) {
+//   return state.map(entry => {
+//     return entry.value < -getLimit(limits, entry.user)
+//       ? { ...entry, flag: 'limit' }
+//       : entry;
+//   });
+//   // for (const entry of newBudget3)
+//   //   if (entry.value < -getLimit(limits, entry.user)) entry.flag = 'limit';
+// };
+
+const checkExpenses = (state, limits) =>
+  state.map(entry =>
+    entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : entry
+  );
+// for (const entry of newBudget3)
+//   if (entry.value < -getLimit(limits, entry.user)) entry.flag = 'limit';
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget);
 
 console.log(budget);
 
-const logBigExpenses = function (bigLimit) {
-  let output = '';
-  for (const entry of budget) {
-    output += entry.value <= -bigLimit ? `${entry.description.slice(-2)}/` : '';
+const logBigExpenses = function (state, bigLimit) {
+  const bigExpenses = state
+    .filter(entry => entry.value <= -bigLimit)
+    .map(entry => entry.description.slice(-2))
+    .join('/');
+  // .reduce((str, cur) => `${str} / ${cur.description.slice(-2)}`, '');
 
-    output = output.slice(0, -2); // Remove last '/ '
-    console.log(output);
-  }
+  console.log(bigExpenses);
+  // let output = '';
+  // for (const entry of budget) {
+  //   output += entry.value <= -bigLimit ? `${entry.description.slice(-2)}/` : '';
+
+  //   output = output.slice(0, -2); // Remove last '/ '
+  //   console.log(output);
+  // }
 };
 console.log(budget);
-logBigExpenses(500);
+logBigExpenses(finalBudget, 500);
